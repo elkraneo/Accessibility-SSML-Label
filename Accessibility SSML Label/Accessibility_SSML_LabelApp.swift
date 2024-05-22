f226ee24d58f0b9e4742ff8f75b3198a1e1c5f31
Cristian Díaz <cristian@210x7.io>
2024-05-22 18:52:58 +0200

    Initial Commit

---
+++ Accessibility SSML Label/Accessibility_SSML_LabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_SSML_LabelApp.swift
+//  Accessibility SSML Label
+//
+//  Created by Cristian Díaz on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_SSML_LabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
